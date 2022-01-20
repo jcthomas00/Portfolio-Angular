acd64c815356f3cae7bcfed381175e0aef222641
jacob <jacob@jacobs-mbp.lan>
2022-01-20 14:52:12 -0600

    move routes to their components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:AboutComponent},
+  {path: "home", component:AboutComponent},
+  {path: "education", component:EducationComponent},
+  {path: "portfolio", component:PortfolioComponent},
+  {path: "resume", component:ResumeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    IntroComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
